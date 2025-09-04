dcb7990370769353f53822a0322f3e9cb72f9395
Vedant <65482974+vedantkhandelwal2007@users.noreply.github.com>
2025-09-04 15:10:39 +0530

    21/8/2025

---
+++ Program1.c
@@ -0,0 +1,18 @@
+// Q1: Write a program to input two numbers and display their sum.
+
+#include <stdio.h>
+
+int main() {
+	
+	int num1,num2,sum;
+	
+	printf("Enter value of num1: ");
+	scanf("%d",&num1);
+	
+	printf("Enter value of num2: ");
+	scanf("%d",&num2);
+	
+	printf("The sum of the given two numbers are: %d",num1+num2);
+	
+	return 0;
+}

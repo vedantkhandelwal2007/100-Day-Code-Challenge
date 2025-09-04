3b828ab8540eed00e245cb385c0fec07406e2126
Vedant <65482974+vedantkhandelwal2007@users.noreply.github.com>
2025-09-04 23:01:53 +0530

    4/9/2025

--- Program29.c
+++ Program29.c
@@ -4,14 +4,15 @@
 
 int main()
 {
-	int n;
-	printf("Enter the number %d",n);
-	scanf("%d",&n);
-	int fact=1,i=0;
-	for(i=1;i<=n;i++)
-	{
-		fact = fact*i;
-	}
-	printf("Factorial of %d is: %d", n,fact);
-	return 0;
+    int n;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    int fact = 1, i = 0;
+    for (i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+    printf("Factorial of %d is: %d", n, fact);
+    return 0;
 }
+

0e7a06a0feed312eba307f9ffadfc8d2b62cdc22
Vedant <65482974+vedantkhandelwal2007@users.noreply.github.com>
2025-09-04 20:32:35 +0530

    3/9/2025

---
+++ Program27.c
@@ -0,0 +1,18 @@
+// Q27: Write a program to print the sum of the first n odd numbers.
+
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	printf("Enter value for n:");
+	scanf("%d",&n);
+	
+	int i,sum=0,odd =1;
+	for(i=1;i<=n;i++)
+	{
+		sum = sum + odd;
+        odd = odd + 2;	
+	}
+	printf("Sum is: %d",sum);
+	return 0;

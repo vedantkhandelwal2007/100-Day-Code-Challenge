74283a731b38652f28e0b5a720fa6beb9d81e2ed
Vedant Khandelwal <65482974+vedantkhandelwal2007@users.noreply.github.com>
2025-09-07 15:18:29 +0530

    5/9/2025

--- Program32.c
+++ Program32.c
@@ -1,21 +1,26 @@
 // Q32: Write a program to check if a number is a palindrome.
 
-#include <stdio.h>
-
-int main() {
-    int num, original, reversed = 0, remainder;
-    scanf("%d", &num);
-    original = num;
-
-    while (num != 0) {
-        remainder = num % 10;
-        reversed = reversed * 10 + remainder;
-        num = num / 10;
-    }
-    if (original == reversed)
-        printf("Palindrome\n");
-    else
-        printf("Not palindrome\n");
-
-    return 0;
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("Enter a Number:");
+	scanf("%d",&num);
+	int temp=num;
+	int sum,n;
+	while(num>0)
+	{
+		n=num%10;
+		sum=(sum*10)+n;
+		num=num/10;
+	}
+	if (sum==temp)
+	{
+		printf("IT IS A PALINDROME.");
+	}
+	else
+	{
+		printf("IT IS NOT A PALINDROME.");
+	}
+	return 0;	
 }

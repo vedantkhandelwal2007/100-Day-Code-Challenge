bfad7a2b638f672483a4740651701aa3997c26b2
Vedant Khandelwal <65482974+vedantkhandelwal2007@users.noreply.github.com>
2025-09-07 15:08:43 +0530

    3/9/2025

--- Program27.c
+++ Program27.c
@@ -4,16 +4,16 @@
 
 int main()
 {
-	int n;
-	printf("Enter value for n:");
-	scanf("%d",&n);
-	
-	int i,sum=0,odd =1;
-	for(i=1;i<=n;i++)
-	{
-		sum = sum + odd;
-        odd = odd + 2;	
-	}
-	printf("Sum is: %d",sum);
-	return 0;
-
+    int n;
+    printf("Enter value for n:");
+    scanf("%d",&n);
+    
+    int i, sum = 0, odd = 1;
+    for(i = 1; i <= n; i++)
+    {
+        sum = sum + odd;
+        odd = odd + 2;    
+    }
+    printf("Sum is: %d", sum);
+    return 0;
+}

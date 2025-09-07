b650bd613317649f37bb862fcc3fce0a529d7d7e
Vedant Khandelwal <65482974+vedantkhandelwal2007@users.noreply.github.com>
2025-09-07 14:43:28 +0530

    29/8/2025

--- Program20.c
+++ Program20.c
@@ -1,70 +1,37 @@
-// Q21: Write a program to display the month name and number of days using switch-case for a given month number.
+// Q20: Write a program to display the day of the week based on a number (1–7) using switch-case.
 
 #include <stdio.h>
 
 int main() {
-	int month,year;
-	
-	printf("Enter Month of the year to see corresponding month and number of days: ");
-	scanf("%d",&month);
-	
-	printf("Enter Year: ");
-	scanf("%d",&year);
-	
-	if(month>=1 && month<=12)
-	{
-		switch(month)
-		{
-			case 1:
-				printf("Januray,31");
-				break;
-			case 2:
-				if(year%4==0)
-				{
-					printf("February,29");
-					break;
-				}
-				else
-				{
-					printf("February,28");
-					break;
-				}
-			case 3:
-				printf("March,31");
-				break;
-			case 4:
-				printf("April,30");
-				break;
-			case 5:
-				printf("May,31");
-				break;
-			case 6:
-				printf("June,30");
-				break;
-			case 7:
-				printf("July,31");
-				break;
-			case 8:	
-				printf("August,31");
-				break;
-			case 9:
-				printf("September,30");
-				break;
-			case 10:
-				printf("October,31");
-				break;
-			case 11:
-				printf("November,30");
-				break;
-			case 12:
-				printf("December,31");
-				break;
-		}
-	}
-	else 
-	{
-		printf("Enter valid number for months");
-	}
-	
-	return 0;
+    int day;
+    printf("Enter a number (1-7): ");
+    scanf("%d", &day);
+
+    switch(day) {
+        case 1:
+            printf("Monday\n");
+            break;
+        case 2:
+            printf("Tuesday\n");
+            break;
+        case 3:
+            printf("Wednesday\n");
+            break;
+        case 4:
+            printf("Thursday\n");
+            break;
+        case 5:
+            printf("Friday\n");
+            break;
+        case 6:
+            printf("Saturday\n");
+            break;
+        case 7:
+            printf("Sunday\n");
+            break;
+        default:
+            printf("Invalid input! Please enter a number between 1 and 7.\n");
+    }
+
+    return 0;
 }

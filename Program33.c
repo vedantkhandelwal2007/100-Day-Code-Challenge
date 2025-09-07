ad024c780ce5f443aeeb10cea6be5b45eae57fe7
Vedant Khandelwal <65482974+vedantkhandelwal2007@users.noreply.github.com>
2025-09-07 15:20:42 +0530

    6/9/2025

--- Program33.c
+++ Program33.c
@@ -1,29 +1,27 @@
 // Q33: Write a program to check if a number is an Armstrong number.
 
-#include <stdio.h>
+#include<stdio.h>
 
-int main(){
-  
-    int num,temp,digit,n=0,sum=0;
-    scanf("%d",&num);
-  
-    temp=num;
-    while(temp>0){
-      n=n+1;
-      temp=temp/10;
-    }
-    temp=num;
-    while(temp>0){
-        digit=temp%10;
-        int power=1,i=0;
-        while(i<n){
-          power=power*digit;
-          i=i+1;
-        }
-        sum=sum+power;
-        temp=temp/10;
-    }
-    if(sum==num)printf("Armstrong");
-    else printf("Not Armstrong");
-    return 0;
+int main()
+{
+	int num;
+	printf("Enter a Number: ",num);
+	scanf("%d",&num);
+	int temp=num;
+	int sum=0,n;
+	while(num>0)
+	{
+		n=num%10;
+		sum=sum+(n*n*n);
+		num=num/10;
+	}
+	if (sum==temp)
+	{
+		printf("IT IS ARMSTRONG.");
+	}
+	else
+	{
+		printf("IT IS NOT ARMSTRONG.");
+	}
+	return 0;	
 }

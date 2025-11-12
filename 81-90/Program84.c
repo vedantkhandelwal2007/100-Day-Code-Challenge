57d96849a7974d1aaea7bed606ebcf71c9f10f2c
Vedant Khandelwal <65482974+vedantkhandelwal2007@users.noreply.github.com>
2025-11-12 11:34:09 +0530

    1/10/2025

---
+++ 81-90/Program84.c
@@ -0,0 +1,20 @@
+//Q84: Convert a lowercase string to uppercase without using built-in functions.
+
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    fgets(str, sizeof(str), stdin); 
+
+    int i;
+    for (i = 0; str[i] != '\0'; i++) {
+        char ch = str[i];
+        
+        if (ch >= 'a' && ch <= 'z') {
+            ch = ch - 32;
+        }
+        printf("%c", ch);
+    }
+
+    return 0;
+}

e1d33b24ad5586faa44af2c2be02ebbdafcdd675
Vedant Khandelwal <65482974+vedantkhandelwal2007@users.noreply.github.com>
2025-11-28 17:09:06 +0530

    30/10/2025

--- 121- 130/Program121.c
+++ 121- 130/Program121.c
@@ -1,4 +1,4 @@
-// Q121: Write a C program that creates a text file named info.txt in write mode. 
+// Q121: Write a C program that cre1tes a text file named info.txt in write mode. 
 //The program should take the user’s name and age as input, and 
 //write them to the file using fprintf(). After writing,
 // display a message confirming that the data was successfully saved.
